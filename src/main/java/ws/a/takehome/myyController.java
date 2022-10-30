021762370c694b676a48a0f37d0e04571968aca2
ERDA WYNE <erdawynee1@gmail.com>
2022-10-30 23:00:18 +0700

    Memperbaiki error

--- src/main/java/ws/a/takehome/myyController.java
+++ src/main/java/ws/a/takehome/myyController.java
@@ -5,6 +5,11 @@
  */
 package ws.a.takehome;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,8 +51,28 @@ public class myyController {
                 
                 return "view";
                 
+    } 
+    
+    public class ImageUpload {
+    @RequestMapping(value = "getimage/{photo}", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<ByteArrayResource> getImage(@PathVariable("photo")String photo) throws IOException{
+        if (!photo.equals("")|| photo !=null) {
+            try{
+                Path filename = Paths.get("uploads",photo);
+                byte[] buffer = Files.readAllBytes(filename);
+                ByteArrayResource byteArrayResource = new ByteArrayResource(buffer);
+                return ResponseEntity.ok()
+                        .contentLength(buffer.length)
+                        .contentType(MediaType.IMAGE_PNG.parseMediaType("image/png"))
+                        .body(byteArrayResource);
+            }catch (Exception e) {
+            }
+            
+        }
+        return ResponseEntity.badRequest().build();
+    }
+   
     }
     
-                
-
 }
